a9d9d33eaafa8fb0b702e3be63127b88e421207f
cedric <cedricgo@live.fr>
2017-12-21 15:48:35 +0100

    [MOD]le server.go, import de mon package et utilisation d'une func

--- www/server.go
+++ www/server.go
@@ -1,10 +1,14 @@
 package main
 
+//!!! pour importer nos packages persos, ils doivent se trouver dans le gopath ou le goroot.
+//il faut donc faire une copie des packages dans /Www/src et les mettre dans le src du gopath
 import (
+	cedPack "cedricPackage"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./HTTP")))
 	http.ListenAndServe(":8000", nil)
+	cedPack.StartModule()
 }
