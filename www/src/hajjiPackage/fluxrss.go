e2f8238bd0f7507f55a403e7934533e94302b3c2
Hajji Mohamed <hajjimohamed12@hotmail.com>
2017-12-22 11:53:43 +0100

    [ADD] mon package

---
+++ www/src/hajjiPackage/fluxrss.go
@@ -0,0 +1,31 @@
+package hajjiPackage
+
+import ( 
+	"fmt"
+	"github.com/mmcdole/gofeed"
+)
+
+func fluxrss() {
+
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL("https://www.lemonde.fr/rss/une.xml" )
+	fmt.Println(feed.Title)
+	fmt.Println(feed.Link)
+	fmt.Println(feed.Items[0].Title)
+        fmt.Println(feed.Items[0].Description)
+        fmt.Println(feed.Items[0].Link)
+//         fmt.Println(feed.Items[0].Enclosur)
+    //objet rss vide (il parse par rapport au type de la variable qu'on lui donne)
+        for _, item := range feed.Items { // for range prend tableau des item de en haut et
+        //et il le parcourt (indice et la valeur) comme ci on avait for key qui va  stocker
+        //l'indice et l'element (range parcourt tout le tableau)
+                fmt.Println("Title: ", item.Title)
+                fmt.Println("Description: ", item.Description)
+                fmt.Println("Link: ", item.Link)
+                fmt.Println("PubDate: ", item.Published)
+		for _, enclosure := range item.Enclosures {
+		fmt.Println("Enclosure", enclosure.URL)
+		}
+                }
+
+}
