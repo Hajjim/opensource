5a86ad5a4f7255007a2888c1932094f238ef63d8
Dbelataris <belataris@outlook.fr>
2017-12-24 19:44:01 +0100

    Restructuration de mon module en package

--- www/goFiles/BelatarisGoFiles/main.go
+++ www/goFiles/BelatarisGoFiles/main.go
@@ -1,33 +1,38 @@
 package main
 
 import (
-	. "BelatarisPackage"
+	BelPack "BelatarisPackage"
 	"fmt"
 	"html/template"
 	"log"
 	"os"
-
-	"github.com/Jeffail/gabs"
 )
 
+type BelData struct {
+	MatchesJournee  BelPack.Journee
+	JourneeActuelle string
+}
+
+var Bel BelData
+
 func main() {
-	var jou Journee
+
 	tpl, err := template.ParseFiles("tpl.gohtml")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	currentMatchday := GetCurrentMatchday()
-	/*url := "http://api.football-data.org/v1/competitions/445/fixtures?timeFrameStart=" + dateBeforeNow + "&timeFrameEnd=" + dateNow*/
-	url := "http://api.football-data.org/v1/competitions/445/fixtures?matchday=" + currentMatchday
-	jsonParsed, _ := gabs.ParseJSON(GetDataAPI(url)) //Récupération des données JSON
+	BelPack.GetFootballData()
+	Bel = BelData{
+		MatchesJournee:  BelPack.Jou,
+		JourneeActuelle: BelPack.CurrentMatchday}
 
-	fmt.Println(currentMatchday)
-	fmt.Println(GetNbrsDeMatch(jsonParsed))
-	jou.AddMatch(jsonParsed) //Ajout de mes données JSON dans mon objet journee
-	fmt.Println(jou.Matches[0].Date)
+	fmt.Println(Bel.JourneeActuelle)
+	//fmt.Println(GetNbrsDeMatch(jsonParsed))
+
+	fmt.Println(Bel.MatchesJournee.Matches[0].Date)
 	Output, err := os.Create("MyPage.html")
 	if err != nil {
 		log.Println("Erreur lors de la création du fichier", err)
 	}
-	tpl.Execute(Output, jou.Matches)
+	tpl.Execute(Output, Bel)
 }
